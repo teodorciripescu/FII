b62156af777cf995d4560dec270eb21abacf6d15
teodorciripescu <teodor.ciripescu@gmail.com>
2018-12-11 02:29:27 +0200

    progress on matrix overlap

--- AN 1 SEM 1/IP/PROIECT/proiectul_meu_drag.cpp
+++ AN 1 SEM 1/IP/PROIECT/proiectul_meu_drag.cpp
@@ -4,7 +4,7 @@
 #include <fstream>
 
 #define PREVIEW_SIZE 3
-#define BOARD_SIZE 4
+#define BOARD_SIZE 7
 #define P1_COLOR GREEN
 #define P2_COLOR YELLOW
 #define NEUTRAL_COLOR WHITE
@@ -18,7 +18,7 @@ struct matrixCoords{
 
 //va trebui o functie care verifica la fiecare mutare starea jocului(daca este onGoing/s-a terminat)
 
-void loadTable();
+void loadTable();void printB();
 void drawMatrix(int top, int left, int previewSquares,int laturaPreview);
 void drawBoardMatrix(int B[BOARD_SIZE+3][BOARD_SIZE+3], matrixCoords M[BOARD_SIZE][BOARD_SIZE]);
 void generateCoordsForMatrix(matrixCoords M[BOARD_SIZE][BOARD_SIZE], int squares, int top, int left, int latura);
@@ -28,8 +28,8 @@ void drawPreviewMatrix(int L[PREVIEW_SIZE][PREVIEW_SIZE], matrixCoords M[PREVIEW
 
 matrixCoords boardCoords[BOARD_SIZE][BOARD_SIZE], previewCoordsL1[PREVIEW_SIZE][PREVIEW_SIZE], previewCoordsL2[PREVIEW_SIZE][PREVIEW_SIZE];
 int previewL1top,previewL1left,previewL2top,previewL2left;
-int BOARD[7][7],latura,L1[PREVIEW_SIZE][PREVIEW_SIZE]={1,0,0,1,0,0,1,1,0},L2[PREVIEW_SIZE][PREVIEW_SIZE]={0,1,1,0,0,1,0,0,1},turn;
-
+int BOARD[BOARD_SIZE+3][BOARD_SIZE+3],latura,L1[PREVIEW_SIZE][PREVIEW_SIZE]={0,1,0,0,1,0,0,1,1},L2[PREVIEW_SIZE][PREVIEW_SIZE]={1,1,0,0,1,0,0,1,0},turn;
+int boardTop, boardLeft, boardWidth, boardHeight, boardLatura, boardLine, boardColumn;
 int previewSquares=3, laturaPreview;
 
 bool gata;
@@ -44,6 +44,7 @@ void initiateCanvas(int width, int height, int squares){
     clearviewport();
     latura = width/squares;
     setcolor(OUTLINE);
+    boardTop = top; boardLeft = left; boardWidth = width; boardHeight = height; boardLatura = latura;
     drawMatrix(top,left,squares,latura);
     generateCoordsForMatrix(boardCoords,squares,top,left,latura);
     drawBoardMatrix(BOARD,boardCoords);
@@ -66,8 +67,13 @@ void initiateCanvas(int width, int height, int squares){
     top = getmaxx() - laturaPreview*(previewSquares+3);
     previewL2top=top;previewL2left=left;
     drawMatrix(previewL2top, previewL2left, previewSquares, laturaPreview);
+    printB();
+    //aici crapa BOARD
     generateCoordsForPreviewMatrix(previewCoordsL2, previewSquares, previewL2top, previewL2left, laturaPreview);
+    //
+    printB();
     drawPreviewMatrix(L2,previewCoordsL2,P2_COLOR);
+    loadTable();
 }
 void drawMatrix(int top, int left, int squares, int latura){
     int i;
@@ -81,7 +87,7 @@ void drawMatrix(int top, int left, int squares, int latura){
 }
 void generateCoordsForMatrix(matrixCoords M[BOARD_SIZE][BOARD_SIZE], int squares, int top, int left, int latura){
     int i,j;
-    setcolor(COLOR(255,100,0));
+    setcolor(OUTLINE);
     for(i=1;i<=squares+1;i++)
         for(j=1;j<=squares+1;j++){
             M[i][j].x1 = top + latura*(i-1);
@@ -184,7 +190,7 @@ void drawPreviewMatrix(int L[PREVIEW_SIZE][PREVIEW_SIZE], matrixCoords M[PREVIEW
 void update(){
     char c;
     c = (char) getch();
-
+    //cout<<c;
     switch(c){
 
         //PLAYER 1 INPUT
@@ -192,25 +198,25 @@ void update(){
         if(!turn%2)flipPreviewMatrix(L1,previewCoordsL1,P1_COLOR);
         break;
         case 'A':
-        if(!turn%2)rotatePreviewMatrixClockwise(L1,previewCoordsL1,P1_COLOR);
+        if(!turn%2)rotatePreviewMatrixCounterClockwise(L1,previewCoordsL1,P1_COLOR);
         break;
         case 'S':
         if(!turn%2)flipPreviewMatrix(L1,previewCoordsL1,P1_COLOR);
         break;
         case 'D':
-        if(!turn%2)rotatePreviewMatrixCounterClockwise(L1,previewCoordsL1,P1_COLOR);
+        if(!turn%2)rotatePreviewMatrixClockwise(L1,previewCoordsL1,P1_COLOR);
         break;
         case 'w':
         if(!turn%2)flipPreviewMatrix(L1,previewCoordsL1,P1_COLOR);
         break;
         case 'a':
-        if(!turn%2)rotatePreviewMatrixClockwise(L1,previewCoordsL1,P1_COLOR);
+        if(!turn%2)rotatePreviewMatrixCounterClockwise(L1,previewCoordsL1,P1_COLOR);
         break;
         case 's':
         if(!turn%2)flipPreviewMatrix(L1,previewCoordsL1,P1_COLOR);
         break;
         case 'd':
-        if(!turn%2)rotatePreviewMatrixCounterClockwise(L1,previewCoordsL1,P1_COLOR);
+        if(!turn%2)rotatePreviewMatrixClockwise(L1,previewCoordsL1,P1_COLOR);printB();
         break;
 
         //PLAYER 2 INPUT
@@ -218,20 +224,86 @@ void update(){
         if(turn%2){flipPreviewMatrix(L2,previewCoordsL2,P2_COLOR);}
         break;
         case KEY_LEFT:
-        if(turn%2)rotatePreviewMatrixClockwise(L2,previewCoordsL2,P2_COLOR);
+        if(turn%2)rotatePreviewMatrixCounterClockwise(L2,previewCoordsL2,P2_COLOR);
         break;
         case KEY_DOWN:
         if(turn%2)flipPreviewMatrix(L2,previewCoordsL2,P2_COLOR);
         break;
         case KEY_RIGHT:
-        if(turn%2)rotatePreviewMatrixCounterClockwise(L2,previewCoordsL2,P2_COLOR);
+        if(turn%2)rotatePreviewMatrixClockwise(L2,previewCoordsL2,P2_COLOR);
         break;
     }
     printMatrix(L1,3);
     //cand e finalizata mutarea punem turn++;
+
+}
+void printB(){
+    for(int i=0;i<6;i++){
+        for(int j=0;j<6;j++)cout<<BOARD[i][j]<<" ";
+        cout<<endl;
+        }
 }
 
+bool canMakeMove(int givenX,int givenY,int L[3][3],int playerNmb,int opponentNmb){
+    int i, j, samePosEncountered=0;
+    int iBoard = givenX-1, jBoard = givenY - 1, nBoard = givenX + 1;
+    if(iBoard<0 || jBoard<0) return false;
+    //printB();cout<<endl;printL();cout<<endl;
+    printB();
+    for(i=0; i<3; i++, iBoard++){
+        for(j=0, jBoard = givenY - 1; j<3 && jBoard<=nBoard; j++, jBoard++){
+            //cout<<BOARD[i1][j1]<<" ";
+            if(  (BOARD[iBoard][jBoard]==5
+                ||BOARD[iBoard][jBoard]==3
+                ||BOARD[iBoard][jBoard]==opponentNmb) && L[i][j]==playerNmb) return false;
+            //if(BOARD[i1][j1]==2&&L1[i][j]==1) return false;
+            //if(BOARD[i1][j1]==3&&L1[i][j]==1) return false;
+            if(BOARD[iBoard][jBoard]==playerNmb && L[i][j]==playerNmb){
+                samePosEncountered++;
+                //cout<<samePosEncountered<<"<-sameposencountered\n";
+                if(samePosEncountered==4) return false;}
 
+        }
+        //cout<<endl;
+    }
+    //cout<<BOARD[n1][n1]<<" ";
+    return 1;
+}
+void makeMove(int givenX,int givenY,int L1[3][3],int playerNmb){
+    int i, j, iBoard=givenX-1, jBoard=givenY-1, nBoard=givenX+1;
+        for(int i=0;i<6;i++)
+            for(int j=0;j<6;j++)if(BOARD[i][j]==1)BOARD[i][j]=0;
+
+        for(i=0; i<3; i++, iBoard++){
+            for(j=0, jBoard=givenY-1; j<3 && jBoard<=nBoard; j++, jBoard++){
+                 //cout<<BOARD[iBoard][jBoard]<<" ";
+                //if(BOARD[iBoard][jBoard]==playerNmb && L1[i][j]==0) BOARD[iBoard][jBoard]=0;
+                if(BOARD[iBoard][jBoard]==0 && L1[i][j]==playerNmb) BOARD[iBoard][jBoard]=1;
+            }//cout<<endl;
+            }
+    printB();
+}
+bool delayed=false;
+void click_handler(int x, int y){
+
+
+    if (x>=boardTop && x<=boardTop+boardWidth && y>=boardLeft&&y<=boardLeft+boardHeight)
+       {
+           //cout<<"clicked on board";
+//printB();
+          boardLine=(x-boardTop)/boardLatura+1; boardColumn=(y-boardLeft)/boardLatura+1;
+          if(canMakeMove(boardColumn,boardLine,L1,1,2)){
+                cout<<"caaan doo\n";
+                makeMove(boardColumn,boardLine,L1,1);
+                drawBoardMatrix(BOARD,boardCoords);
+
+          }
+          else cout<<"can't doo\n";
+          //if(BOARD[boardColumn][boardLine]==1)cout<<"AM DAT CLICK ";
+       }
+   // if(delayed){}
+   //     else {delayed=true;delay(500);}
+}
 
 void loadTable(){
     int n;
@@ -240,6 +312,7 @@ void loadTable(){
     for(int i=0;i<n;i++)
         for(int j=0;j<n;j++)f>>BOARD[i][j];
     f.close();
+    printB();
 }
 int main(){
     int x = getmaxwidth(), y=getmaxheight();
@@ -247,75 +320,19 @@ int main(){
     initwindow(x-80,y);
     initiateCanvas(400,400,4);
     printMatrix(L1,3);
+
     //printMatrix(L2,3);
+    //left click mouse handler
+    registermousehandler(WM_LBUTTONDOWN, click_handler);
     gata=false;
-   do
-   {
-      update();
-   }
-   while (!gata);
+       do
+       {
+          update();
+       }
+       while (!gata);
 
     //initiateCanvas((x-80)/4,y/4,4);
     closegraph();
     return 0;
 }
-
-/*
-stances:
-1)
-*
-*
-* *
-1 0 0
-1 0 0
-1 1 0
-2)
-    *
-* * *
-0 0 0
-0 0 1
-1 1 1
-3)
-* *
-  *
-  *
-0 1 1
-0 0 1
-0 0 1
-4)
-* * *
-*
-1 1 1
-1 0 0
-0 0 0
-//now flipped:
-5)
-  *
-  *
-* *
-0 1 0
-0 1 0
-1 1 0
-6)
-* * *
-    *
-0 0 0
-1 1 1
-0 0 1
-7)
-* *
-*
-*
-1 1 0
-1 0 0
-1 0 0
-8)
-*
-* * *
-1 0 0
-1 1 1
-0 0 0
-
-*/
-
 
--- AN 1 SEM 1/IP/PROIECT/suprapunere_matrice.cpp
+++ AN 1 SEM 1/IP/PROIECT/suprapunere_matrice.cpp
@@ -23,6 +23,56 @@ printL(){
         cout<<endl;
         }
 }
+bool canMakeMove(int givenX,int givenY,int L1[3][3],int playerNmb,int opponentNmb){
+    int i, j, samePosEncountered=0;
+    int iBoard = givenX-1, jBoard = givenY - 1, nBoard = givenX + 1;
+    if(iBoard<0 || jBoard<0) return false;
+    //printB();cout<<endl;printL();cout<<endl;
+    for(i=0; i<3; i++, iBoard++){
+        for(j=0, jBoard = givenY - 1; j<3 && jBoard<=nBoard; j++, jBoard++){
+            //cout<<BOARD[i1][j1]<<" ";
+            if(  (BOARD[iBoard][jBoard]==5
+                ||BOARD[iBoard][jBoard]==3
+                ||BOARD[iBoard][jBoard]==opponentNmb) && L1[i][j]==playerNmb) return false;
+            //if(BOARD[i1][j1]==2&&L1[i][j]==1) return false;
+            //if(BOARD[i1][j1]==3&&L1[i][j]==1) return false;
+            if(BOARD[iBoard][jBoard]==playerNmb && L1[i][j]==playerNmb){
+                samePosEncountered++;
+                //cout<<samePosEncountered<<"<-sameposencountered\n";
+                if(samePosEncountered==4) return false;}
+
+        }
+        //cout<<endl;
+    }
+    //cout<<BOARD[n1][n1]<<" ";
+    return 1;
+}
+void makeMove(int givenX,int givenY,int L1[3][3],int playerNmb){
+    int i, j, iBoard=givenX-1, jBoard=givenY-1, nBoard=givenX+1;
+        for(int i=0;i<6;i++)
+            for(int j=0;j<6;j++)if(BOARD[i][j]==1)BOARD[i][j]=0;
+
+        for(i=0; i<3; i++, iBoard++){
+            for(j=0, jBoard=givenY-1; j<3 && jBoard<=nBoard; j++, jBoard++){
+                 cout<<BOARD[iBoard][jBoard]<<" ";
+                //if(BOARD[iBoard][jBoard]==playerNmb && L1[i][j]==0) BOARD[iBoard][jBoard]=0;
+                if(BOARD[iBoard][jBoard]==0 && L1[i][j]==playerNmb) BOARD[iBoard][jBoard]=1;
+            }cout<<endl;
+            }
+    printB();
+}
+
+int main(){
+loadTable();
+int givenX=3, givenY=1;
+bool ableToMove = canMakeMove(givenX,givenY,L1,1,2);
+cout<<endl<<ableToMove<<endl;
+if(ableToMove) makeMove(givenX, givenY,L1,1);
+return 0;
+}
+
+
+/*
 bool canMakeMove(int givenX,int givenY,int L1[3][3]){
 int i,j;int test=0,ok=0;int isDifPos=0;int samePosEncountered=0;
 
@@ -35,13 +85,14 @@ for(i=0;i<3;i++)
     for(j=0;j<3;j++){
         cout<<"i1="<<i1<<", j1="<<j1<<endl;
         cout<<"test"<<test++<<endl;
-        if(BOARD[i1][j1]==5&&L1[i][j]==1)return 0;
-        if(BOARD[i1][j1]==2&&L1[i][j]==1) return 0;
-        if(BOARD[i1][j1]==3&&L1[i][j]==1) return 0;
+        if(BOARD[i1][j1]==5&&L1[i][j]==1)return false;
+        if(BOARD[i1][j1]==2&&L1[i][j]==1) return false;
+        if(BOARD[i1][j1]==3&&L1[i][j]==1) return false;
         if(BOARD[i1][j1]==1)samePosEncountered++;
         if(samePosEncountered==4){
                 //cannot move fix
-                return 0;
+
+                return false;
         }
         if((BOARD[i1][j1]==0&&L1[i][j]==1)){
             cout<<"ok"<<++ok<<"\n";
@@ -65,9 +116,4 @@ for(int i=0;i<6;i++){
         }
 printB();
 }
-int main(){
-loadTable();
-int givenX=3, givenY=1;
-canMakeMove(givenX,givenY,L1);
-return 0;
-}
+*/
